203097973fc17f08a956004ea34c4c57888c3b55
Ian Gornall <igornall@clearpointstrategy.com>
2022-06-15 13:23:45 -0400

    fix import statement matcher

--- utils/getImportStatementList.js
+++ utils/getImportStatementList.js
@@ -1,8 +1,8 @@
 import logger from "../logger";
 
 let getImportStatementList = (fileText) => {
-	let firstStatement = fileText.match(/^import .+?";/);
-	let statementList = fileText.match(/(?<=\n)import [\s\S]+?";/g);
+	let firstStatement = fileText.match(/^import .+?".+?"/);
+	let statementList = fileText.match(/(?<=\n)import [\s\S]+?".+?"/g);
 	logger.debug("--- IMPORT STATEMENT LIST ---");
 	logger.debug([...(firstStatement || []), ...(statementList || [])]);
 	return [...(firstStatement || []), ...(statementList || [])];
